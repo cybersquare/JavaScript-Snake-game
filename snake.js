a70e947ce074804cebf4257d1822657f997dab59
Cyber Square <70192724+cybersquare@users.noreply.github.com>
2020-09-28 12:35:09 +0530

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,171 @@
+//Select the canvas
+const cvs = document.getElementById("snake");
+//getContext('2d') method, has properties and methods that allow us to draw and do different things on the canvas.
+const ctx = cvs.getContext("2d"); 
+
+// create the unit
+const box = 32; //Ibox here is like a unit, snake is a square, it's width and height both equal to 32px by default.
+
+// load images
+
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
+// load audio files
+
+let dead = new Audio();
+let eat = new Audio();
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
+
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";
+
+// create the snake. the snake position is given using the box unit
+
+let snake = [];
+
+snake[0] = {
+    x : 9 * box,
+    y : 10 * box
+};
+
+// create the food. the food position is given using the box unit.
+
+let food = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+
+// create the score var
+
+let score = 0;
+
+//control the snake
+
+let d;
+
+//To control our snake we will need to add an eventListener to our document. 
+
+document.addEventListener("keydown",direction); //whenever the player press a key on the keyboard, the function - direction will fire up.
+
+function direction(event){
+    let key = event.keyCode; //every key on the keyboard has a code
+    if( key == 37 && d != "RIGHT"){  // to the snake move to the left , we need to make sure, that our snake isn't going to the right.
+        left.play(); // playing the sound track
+        d = "LEFT";
+    }else if(key == 38 && d != "DOWN"){
+        d = "UP";
+        up.play();
+    }else if(key == 39 && d != "LEFT"){
+        d = "RIGHT";
+        right.play();
+    }else if(key == 40 && d != "UP"){
+        d = "DOWN";
+        down.play();
+    }
+}
+
+// check collision function. 
+//to check the snake hits, we need to check if all the squares from the snake array (except the head), doesn't have the same coordinates as the head.
+function collision(head,array){
+    for(let i = 0; i < array.length; i++){
+        if(head.x == array[i].x && head.y == array[i].y){
+            return true;
+        }
+    }
+    return false;
+}
+
+// draw everything to the canvas
+
+function draw(){
+    
+    ctx.drawImage(ground,0,0);
+    
+    for( let i = 0; i < snake.length ; i++){
+        ctx.fillStyle = ( i == 0 )? "green" : "white";// the snake head will be in green color and the remaining part is in white
+        ctx.fillRect(snake[i].x,snake[i].y,box,box);
+        
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+    }
+    
+    ctx.drawImage(foodImg, food.x, food.y);
+    
+    // old head position
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    
+    // which direction
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+    
+    // if the snake eats the food
+    if(snakeX == food.x && snakeY == food.y){
+        score++; // increse the score
+        eat.play();
+        food = {
+            //The food is then placed in any random position - (x,y)
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+        // we don't remove the tail
+    }else{
+        // remove the tail
+        snake.pop();
+    }
+    
+    // add new Head
+    
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+    
+    // game over
+    
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
+        clearInterval(game);
+        dead.play();
+    }
+    
+    snake.unshift(newHead);
+    
+    ctx.fillStyle = "white";
+    ctx.font = "45px Changa one";
+    ctx.fillText(score,2*box,1.6*box);
+}
+
+// call draw function every 100 ms
+
+let game = setInterval(draw,100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
